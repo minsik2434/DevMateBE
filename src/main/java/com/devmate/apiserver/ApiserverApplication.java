87496442ec3ca0167ad31312705936d4eab3a872
minsik2434 <minsik2434@gmail.com>
2024-05-10 15:23:05 +0900

    Feat : querydsl 설정 추가

--- src/main/java/com/devmate/apiserver/ApiserverApplication.java
+++ src/main/java/com/devmate/apiserver/ApiserverApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ApiserverApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(ApiserverApplication.class, args);
 	}
-
 }
