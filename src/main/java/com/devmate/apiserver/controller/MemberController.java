1b993d069f9e811e4c612c6a9945e435780128fa
minsik2434 <minsik2434@gmail.com>
2024-05-23 19:15:51 +0900

    Feat : swagger 설정 추가

--- src/main/java/com/devmate/apiserver/controller/MemberController.java
+++ src/main/java/com/devmate/apiserver/controller/MemberController.java
@@ -4,6 +4,7 @@ import com.devmate.apiserver.common.exception.ConfirmPasswordNotMatchException;
 import com.devmate.apiserver.common.exception.DuplicateResourceException;
 import com.devmate.apiserver.common.jwt.JwtToken;
 import com.devmate.apiserver.controller.util.ControllerUtil;
+import com.devmate.apiserver.dto.FailResponseDto;
 import com.devmate.apiserver.dto.SuccessResponseDto;
 import com.devmate.apiserver.dto.member.request.EditProfileDto;
 import com.devmate.apiserver.dto.member.request.MemberRegisterDto;
@@ -11,6 +12,12 @@ import com.devmate.apiserver.dto.member.request.SignInDto;
 import com.devmate.apiserver.dto.member.response.MemberDto;
 import com.devmate.apiserver.service.MemberQueryService;
 import com.devmate.apiserver.service.MemberService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -22,7 +29,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-
+@Tag(name = "회원 컨트롤러", description = "회원 API")
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/members")
@@ -32,12 +39,20 @@ public class MemberController {
     private final MemberQueryService memberQueryService;
     private final ControllerUtil controllerUtil;
 
+
     @GetMapping("test")
     public String test(){
         return "test";
     }
 
-    
+
+    @Operation(summary = "회원 등록", description = "비밀번호와 비밀번호 확인이 일치해야함, 이미 존재하는 ID 사용 불가, 이미 존재하는 닉네임 사용 불가")
+    @ApiResponses({
+            @ApiResponse(responseCode = "201", description = "회원 등록 성공", content = @Content(schema = @Schema(implementation = SuccessResponseDto.class))),
+            @ApiResponse(responseCode = "400", description = "값 검증 실패", content = @Content(schema = @Schema(implementation = FailResponseDto.class))),
+            @ApiResponse(responseCode = "409", description = "중복 리소스" ,content = @Content(schema = @Schema(implementation = FailResponseDto.class))),
+            @ApiResponse(responseCode = "404", description = "존재하지 않는 리소스",content = @Content(schema = @Schema(implementation = FailResponseDto.class)))
+    })
     @PostMapping("/register")
     public ResponseEntity<SuccessResponseDto<MemberDto>> register(@RequestBody @Validated MemberRegisterDto memberRegisterDto){
         if(!memberRegisterDto.getPassword().equals(memberRegisterDto.getConfirmPassword())){
@@ -58,6 +73,12 @@ public class MemberController {
         return ResponseEntity.status(HttpStatus.CREATED).body(successResponse);
     }
 
+    @Operation(summary = "로그인", description = "로그인 성공시 토큰 발급")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "로그인 성공", content = @Content(schema = @Schema(implementation = SuccessResponseDto.class))),
+            @ApiResponse(responseCode = "400", description = "값 검증 실패", content = @Content(schema = @Schema(implementation = FailResponseDto.class))),
+            @ApiResponse(responseCode = "401", description = "로그인 실패",content = @Content(schema = @Schema(implementation = FailResponseDto.class)))
+    })
     @PostMapping("/signin")
     public ResponseEntity<SuccessResponseDto<JwtToken>> signIn(@RequestBody @Validated SignInDto signInDto){
         JwtToken jwtToken = memberService.signIn(signInDto.getLoginId(),signInDto.getPassword());
@@ -67,6 +88,12 @@ public class MemberController {
         return ResponseEntity.ok().body(successResponse);
     }
 
+    @Operation(summary = "회원 탈퇴", description = "인증 토큰 필요")
+    @ApiResponses({
+            @ApiResponse(responseCode = "204", description = "탈퇴 성공"),
+            @ApiResponse(responseCode = "401", description = "인증에러", content = @Content(schema = @Schema(implementation = FailResponseDto.class))),
+            @ApiResponse(responseCode = "404", description = "존재하지 않는 리소스",content = @Content(schema = @Schema(implementation = FailResponseDto.class)))
+    })
     @DeleteMapping
     public ResponseEntity<Void> withDraw(Authentication authentication){
         String loginId = controllerUtil.getAuthorizedLoginId(authentication);
@@ -74,6 +101,12 @@ public class MemberController {
         return ResponseEntity.noContent().build();
     }
 
+    @Operation(summary = "회원 수정", description = "인증 토큰 필요")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "수정 성공", content = @Content(schema = @Schema(implementation = SuccessResponseDto.class))),
+            @ApiResponse(responseCode = "400", description = "값 검증 에러", content = @Content(schema = @Schema(implementation = FailResponseDto.class))),
+            @ApiResponse(responseCode = "404", description = "존재하지 않는 리소스",content = @Content(schema = @Schema(implementation = FailResponseDto.class)))
+    })
     @PatchMapping
     public ResponseEntity<SuccessResponseDto<MemberDto>> editProfile(Authentication authentication,
                                               @RequestBody @Validated EditProfileDto editProfileDto){
--- src/main/java/com/devmate/apiserver/dto/SuccessResponseDto.java
+++ src/main/java/com/devmate/apiserver/dto/SuccessResponseDto.java
@@ -1,5 +1,6 @@
 package com.devmate.apiserver.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -8,5 +9,6 @@ import lombok.Setter;
 @Getter
 @Setter
 public class SuccessResponseDto<T> extends ResponseDto{
+    @Schema(description = "데이터", example = "예) {...}")
     private T data;
 }
