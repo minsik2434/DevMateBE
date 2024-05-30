e857f2f4a0ab32785b736db9d9aec68bd3434e7e
minsik2434 <minsik2434@gmail.com>
2024-05-30 19:05:17 +0900

    Feat : 게시물 등록 검증 추가

--- src/main/java/com/devmate/apiserver/dto/post/request/PostRegisterDto.java
+++ src/main/java/com/devmate/apiserver/dto/post/request/PostRegisterDto.java
@@ -1,5 +1,8 @@
 package com.devmate.apiserver.dto.post.request;
 
+import com.devmate.apiserver.common.annotation.UniqueElements;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -8,7 +11,11 @@ import java.util.List;
 @Getter
 @Setter
 public class PostRegisterDto {
+    @NotBlank
     private String title;
+    @NotBlank
     private String content;
+    @UniqueElements
+    @Size(max = 4, message = "Size must be 4 or less")
     private List<String> tags;
 }
