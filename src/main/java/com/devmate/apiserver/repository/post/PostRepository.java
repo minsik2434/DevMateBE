3ab02f3650f157fd08454750c494cc65afb991fb
minsik2434 <minsik2434@gmail.com>
2024-05-31 19:21:20 +0900

    Feat : PostRepository queryDsl 추가

--- src/main/java/com/devmate/apiserver/repository/post/PostRepository.java
+++ src/main/java/com/devmate/apiserver/repository/post/PostRepository.java
@@ -8,7 +8,7 @@ import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
-public interface PostRepository extends JpaRepository<Post, Long> {
+public interface PostRepository extends JpaRepository<Post, Long>, PostRepositoryCustom {
     @Query("select p from Post p join fetch p.member join fetch p.postHashTag ph join fetch ph.hashTag where p.id = :id")
     Optional<Post> findPostAndMemberAndTagsById(@Param("id") Long id);
 }
