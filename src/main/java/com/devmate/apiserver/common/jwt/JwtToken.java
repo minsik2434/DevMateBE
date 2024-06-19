33a70bc40726c0d523b9798c02fd2b361faad6af
minsik2434 <minsik2434@gmail.com>
2024-06-19 15:53:02 +0900

    Feat : EndPoint 별 스웨거 설명 작성

--- src/main/java/com/devmate/apiserver/common/jwt/JwtToken.java
+++ src/main/java/com/devmate/apiserver/common/jwt/JwtToken.java
@@ -1,5 +1,6 @@
 package com.devmate.apiserver.common.jwt;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -8,7 +9,10 @@ import lombok.Setter;
 @AllArgsConstructor
 @Getter
 public class JwtToken {
+    @Schema(description = "GrantType", example = "예) Bearer")
     private String grantType;
+    @Schema(description = "AccessToken", example = "예) 123123.abcderghijklnm=+")
     private String accessToken;
+    @Schema(description = "refreshToken", example = "예) 123123.abcderghijklnm=+")
     private String refreshToken;
 }
--- src/main/java/com/devmate/apiserver/dto/comment/response/CommentDto.java
+++ src/main/java/com/devmate/apiserver/dto/comment/response/CommentDto.java
@@ -2,6 +2,7 @@ package com.devmate.apiserver.dto.comment.response;
 
 import com.devmate.apiserver.domain.Comment;
 import com.devmate.apiserver.domain.Member;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -10,15 +11,21 @@ import java.time.LocalDateTime;
 @Getter
 @Setter
 public class CommentDto {
+    @Schema(description = "댓글 ID", example = "예) 1")
     private Long id;
+    @Schema(description = "댓글 내용", example = "예) 궁금합니다")
     private String content;
+    @Schema(description = "댓글 등록 시간(LocalDateTime)", example = "예) 2024-06-19T14:51:41.67998")
     private LocalDateTime commentDateTime;
+
     private Writer writer;
 
     @Getter
     @Setter
     static class Writer{
+        @Schema(description = "작성자 닉네임", example = "예) 제이드")
         private String nickName;
+        @Schema(description = "작성자 프로필 이미지 URL", example = "예) https://img.test.com")
         private String profileImgUrl;
 
         Writer(Member member){
