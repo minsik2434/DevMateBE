e10e905071156c6a83cf2ce5cf3e567b8d5e4a8e
minsik2434 <minsik2434@gmail.com>
2024-07-05 22:58:08 +0900

    Chore : cors 설정

--- src/main/java/com/devmate/apiserver/common/config/WebSecurityConfig.java
+++ src/main/java/com/devmate/apiserver/common/config/WebSecurityConfig.java
@@ -43,7 +43,7 @@ public class WebSecurityConfig implements WebMvcConfigurer{
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000","https://clone-tving.vercel.app","https://localhost:3000")
+                .allowedOrigins("http://localhost:3000","https://devmate-fe.vercel.app","https://localhost:3000")
                 .allowedMethods("*")
                 .allowCredentials(true);
     }
