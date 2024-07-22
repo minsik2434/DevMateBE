a9412197a389232ed146cd8ade7f5bdb0d0d0443
minsik2434 <minsik2434@gmail.com>
2024-07-22 18:57:41 +0900

    Test : postRepository 테스트 추가

--- src/test/java/com/devmate/apiserver/repository/post/PostRepositoryTest.java
+++ src/test/java/com/devmate/apiserver/repository/post/PostRepositoryTest.java
@@ -4,21 +4,18 @@ import com.devmate.apiserver.domain.*;
 import com.devmate.apiserver.dto.comment.request.CommentRequestDto;
 import com.devmate.apiserver.dto.post.request.PostRequestDto;
 import com.devmate.apiserver.repository.CommentRepository;
+import com.devmate.apiserver.repository.GoodRepository;
 import com.devmate.apiserver.repository.MemberRepository;
-import jakarta.persistence.EntityManager;
-import jakarta.persistence.PersistenceContext;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
 import org.springframework.transaction.annotation.Transactional;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -26,112 +23,150 @@ import static org.assertj.core.api.Assertions.assertThat;
 @Slf4j
 @Transactional
 class PostRepositoryTest {
-//
-//    @Autowired
-//    PostRepository postRepository;
-//    @Autowired
-//    MemberRepository memberRepository;
-//    @Autowired
-//    HashTagRepository hashTagRepository;
-//    @Autowired
-//    CommentRepository commentRepository;
-//
-//    List<Qna> initQna = new ArrayList<>();
-//
-//    @PersistenceContext
-//    EntityManager em;
-//
-//    @BeforeEach
-//    void initData(){
-//        Member member = memberRepository.findById(1L).get();
-//        for(int i=0; i<10; i++){
-//            PostRequestDto postRegisterDto = new PostRequestDto();
-//            postRegisterDto.setTitle("qna"+i);
-//            postRegisterDto.setContent("qna"+i+"번 입니다");
-//            Qna qna = new Qna(member, postRegisterDto);
-//            initQna.add(qna);
-//        }
-//    }
-//
-//    @Test
-//    void findPostAllByParamsTest(){
-//        HashTag hashTag1 = new HashTag("java");
-//        HashTag hashTag2 = new HashTag("spring");
-//        HashTag hashTag3 = new HashTag("jpa");
-//        hashTagRepository.save(hashTag1);
-//        hashTagRepository.save(hashTag2);
-//        hashTagRepository.save(hashTag3);
-//        for(int i=0; i<initQna.size(); i++){
-//            if(i%2==0){
-//                Qna qna = initQna.get(i);
-//                new PostHashTag(qna,hashTag1);
-//            }
-//            else{
-//                Qna qna = initQna.get(i);
-//                new PostHashTag(qna, hashTag1);
-//                new PostHashTag(qna, hashTag2);
-//                for (int j = 0; j < i; j++) {
-//                    qna.addViewCount();
-//                }
-//            }
-//        }
-//        postRepository.saveAll(initQna);
-//
-//        Pageable pageable = PageRequest.of(0,20);
-//        Page<Post> result = postRepository.findPostAllByParam("qna", "viewCount", null, new String[]{"spring"}, pageable);
-//
-//        assertThat(result.getContent()).isNotEmpty();
-//        assertThat(result.getContent().stream().allMatch(item->item.getClass() == Qna.class)).isTrue();
-//        result.getContent().forEach(post ->{
-//            assertThat(post.getPostHashTag().stream().anyMatch(postHashTag -> postHashTag.getHashTag().getName().equals("spring"))).isTrue();
-//        });
-//
-//        assertThat(result.getContent().get(0).getTitle()).isEqualTo("qna9");
-//    }
-//
-//    @Test
-//    void findPostAllByMemberFilterParamTest(){
-//        List<Qna> qnas = postRepository.saveAll(initQna);
-//        Member member1 = memberRepository.findById(1L).get();
-//        Member member2 = memberRepository.findById(2L).get();
-//        CommentRequestDto commentRequestDto = new CommentRequestDto();
-//        commentRequestDto.setComment("qna 댓글 테스트 입니다");
-//        Qna qna = qnas.get(3);
-//        Qna qna1 = qnas.get(4);
-//
-//        Qna qna2 = qnas.get(7);
-//        Qna qna3 = qnas.get(8);
-//        Comment comment = new Comment(member2, qna, commentRequestDto);
-//        Comment comment1 = new Comment(member2, qna1, commentRequestDto);
-//
-//        Comment comment2 = new Comment(member1, qna2, commentRequestDto);
-//        Comment comment3 = new Comment(member1, qna3, commentRequestDto);
-//
-//
-//        commentRepository.save(comment);
-//        commentRepository.save(comment1);
-//        commentRepository.save(comment2);
-//        commentRepository.save(comment3);
-//
-//        Pageable pageable = PageRequest.of(0,20);
-//
-//        //검증 member1이 작성한 게시물들인지 검증
-//        Page<Post> member1PostsByPost = postRepository.findPostAllByMemberFilterParam(member1.getId(), "post", pageable);
-//        assertThat(member1PostsByPost.getTotalElements()).isEqualTo(10);
-//        assertThat(member1PostsByPost.getContent()).containsExactlyInAnyOrderElementsOf(qnas);
-//
-//        //검증 member1이 댓글단 게시글들인지 검증
-//        Page<Post> member1PostsByComment = postRepository.findPostAllByMemberFilterParam(member1.getId(), "comment", pageable);
-//        assertThat(member1PostsByComment.getTotalElements()).isEqualTo(2);
-//        assertThat(member1PostsByComment.getContent()).containsAll(Arrays.asList(qnas.get(7), qnas.get(8)));
-//
-//        //검증 member2가 작성한 게시글인지 검증
-//        Page<Post> member2PostsByPost = postRepository.findPostAllByMemberFilterParam(member2.getId(), "post", pageable);
-//        assertThat(member2PostsByPost.getTotalElements()).isEqualTo(0);
-//        assertThat(member2PostsByPost.getContent()).isEmpty();
-//
-//        Page<Post> member2PostsByComment = postRepository.findPostAllByMemberFilterParam(member2.getId(), "comment", pageable);
-//        assertThat(member2PostsByComment.getTotalElements()).isEqualTo(2);
-//        assertThat(member2PostsByComment.getContent()).containsAll(Arrays.asList(qnas.get(3), qnas.get(4)));
-//    }
+
+    @Autowired
+    PostRepository postRepository;
+    @Autowired
+    MemberRepository memberRepository;
+    @Autowired
+    HashTagRepository hashTagRepository;
+    @Autowired
+    PostHashTagRepository postHashTagRepository;
+    @Autowired
+    CommentRepository commentRepository;
+    @Autowired
+    GoodRepository goodRepository;
+
+    Member member;
+    Post qna;
+    Post community;
+    Post job;
+    PostRequestDto postRequestDto;
+    HashTag hashTag;
+    PostHashTag qnaHashTag;
+    PostHashTag communityHashTag;
+    PostHashTag jobHashTag;
+    Comment qnaComment;
+    CommentRequestDto commentRequestDto;
+    Good qnaGood;
+    @BeforeEach
+    void init(){
+        //Member
+        member = new Member(
+                "test",
+                "test",
+                "테스터",
+                "닉네임",
+                true,
+                "testURL");
+
+        //Post
+        List<String> list = new ArrayList<>();
+        list.add("tag1");
+        postRequestDto = new PostRequestDto();
+        postRequestDto.setTitle("제목");
+        postRequestDto.setContent("내용");
+        postRequestDto.setTags(list);
+        qna = new Qna(member, postRequestDto);
+        community = new Community(member, postRequestDto);
+        job = new JobOpening(member,postRequestDto);
+
+        hashTag = new HashTag("Test");
+
+        qnaHashTag = new PostHashTag(qna,hashTag);
+        communityHashTag = new PostHashTag(community,hashTag);
+        jobHashTag = new PostHashTag(job,hashTag);
+
+
+        commentRequestDto = new CommentRequestDto();
+        commentRequestDto.setComment(("댓글 내용"));
+        qnaComment = new Comment(member,qna,commentRequestDto);
+
+        qnaGood = new Good(member,qna);
+    }
+
+    @Test
+    @DisplayName("post 아이디로 post조회 (fetch Member and Tags)")
+    void findPostByIdTest_fetchMemberAndTags(){
+
+        //Given
+        memberRepository.save(member);
+        postRepository.save(qna);
+        hashTagRepository.save(hashTag);
+        postHashTagRepository.save(qnaHashTag);
+
+        //When
+        Optional<Post> findPost =
+                postRepository.findPostAndMemberAndTagsById(qna.getId());
+
+        //Then
+        assertThat(findPost.isPresent()).isTrue();
+        assertThat(findPost.get().getId()).isEqualTo(qna.getId());
+
+        assertThat(findPost.get().getPostHashTag()).isNotNull();
+        assertThat(findPost.get().getPostHashTag().size()).isGreaterThan(0);
+
+        assertThat(findPost.get().getMember()).isNotNull();
+        assertThat(findPost.get().getMember()).isEqualTo(member);
+    }
+
+    @Test
+    @DisplayName("회원이 작성한 post 아이디로 조회")
+    void findPostTestByPostAndMemberIdTest_fetchMember(){
+        //Given
+        memberRepository.save(member);
+        postRepository.save(qna);
+        hashTagRepository.save(hashTag);
+        postHashTagRepository.save(qnaHashTag);
+
+        //When
+        Optional<Post> findPost =
+                postRepository.findPostAndMemberById(qna.getId(), member.getLoginId());
+
+
+        //Then
+        assertThat(findPost.isPresent()).isTrue();
+        assertThat(findPost.get().getId()).isEqualTo(qna.getId());
+
+        assertThat(findPost.get().getMember()).isNotNull();
+        assertThat(findPost.get().getMember()).isEqualTo(member);
+    }
+
+    @Test
+    @DisplayName("회원이 댓글을 작성한 post 리스트 조회")
+    void findPostByMemberCommentTest(){
+        //Given
+        memberRepository.save(member);
+        postRepository.save(qna);
+        hashTagRepository.save(hashTag);
+        postHashTagRepository.save(qnaHashTag);
+        commentRepository.save(qnaComment);
+
+        //When
+        List<Post> postList = postRepository.findPostByMemberComment(member);
+
+        //Then
+        assertThat(postList).isNotNull();
+        assertThat(postList.size()).isGreaterThan(0);
+        assertThat(postList.get(0)).isEqualTo(qna);
+    }
+
+    @Test
+    @DisplayName("회원이 좋아요한 post 리스트 조회")
+    void findPostByMemberGoodTest(){
+        //Given
+        memberRepository.save(member);
+        postRepository.save(qna);
+        hashTagRepository.save(hashTag);
+        postHashTagRepository.save(qnaHashTag);
+        goodRepository.save(qnaGood);
+
+        //When
+        List<Post> postList = postRepository.findPostsByMemberGood(member);
+
+        //Then
+        assertThat(postList).isNotNull();
+        assertThat(postList.size()).isGreaterThan(0);
+        assertThat(postList.get(0)).isEqualTo(qna);
+    }
 }
